d3c9f578e0fad5be5765b94cb5321c924ba73de6
Ingenium-LiDAR <ingenium.lidar@outlook.com>
2026-07-08 11:26:03 -0500

    add explanatory comments and cd commands before colcon build

--- agent_scripts/Install_SLAM.sh
+++ agent_scripts/Install_SLAM.sh
@@ -8,9 +8,10 @@
 source /opt/ros/jazzy/setup.bash #FK source the version of ros
 
 
-mkdir -p ~/Apps/ndt_omp_ros2/ros2_ws/src 
-mkdir -p ~/Apps/lidarslam_ros2/ros2_ws/src
-
+mkdir -p ~/Apps/ndt_omp_ros2/ros2_ws/src #FK create the directory for the ROS 2 workspace of ndt_omp_ros2, and the subdirectory for its source code
+echo -e "\e[38;5;5m If you got a 'fatal' error saying ros2_ws already exists, do not worry. Everything is OK. \033[0m"
+mkdir -p ~/Apps/lidarslam_ros2/ros2_ws/src #FK create the directory for the ROS 2 workspace of lidarslam_ros2, and the subdirectory for its source code
+echo -e "\e[38;5;5m If you got a 'fatal' error saying ros2_ws already exists, do not worry. Everything is OK. \033[0m"
 
 sudo apt install python3-colcon-common-extensions -y #AB 2026-06-15 added this installer to DAI
 sudo apt install python3-rosdep -y #AB install rosdep, which I guess doens't come  by default! NB! python3-rosdep2 is only for Debian--python3-rosdep is for Ubuntu 
@@ -23,29 +24,38 @@ rosdep update  #AB update rosdep
 #---------------------------------------------INSTALL ndt_omp_ros2---------------------------------------------
 
 
-echo -e "\e[38;5;5m If you got a 'fatal' error saying ros2_ws already exists, do not worry. Everything is OK. \033[0m"
+#FK Note: since the lidarslam_ros2 workspace depends on the ndt_omp_ros2 workspace, ndt_omp_ros2 has to be installed first, which is why this section (INSTALL ndt_omp_ros2) comes before the next section (INSTALL lidarslam_ros2)
 
+#FK Install the ndt_omp_ros2 workspace's source code
 cd ~/Apps/ndt_omp_ros2/ros2_ws/src
-git clone https://github.com/rsasaki0109/ndt_omp_ros2.git -b humble
-cd ndt_omp_ros2
+git clone https://github.com/rsasaki0109/ndt_omp_ros2.git -b humble #FK note: I'm NOT sure about this, but I think that this installs one software package, called ndt_omp_ros2, into the ndt_omp_ros2 workspace
+
+#FK Build the ndt_omp_ros2 workspace
+cd ~/Apps/ndt_omp_ros2/ros2_ws #FK move to the root of the workspace, so that "colcon build" works correctly
 colcon build --executor sequential --cmake-clean-first
 echo -e "\e[38;5;5m If you got depreciation warnings and such, but nothing labeled 'error' or something else really serious, do not worry. Everything is OK. \033[0m"
-#FK source the ros setup script for this application specifically
-source ~/Apps/ndt_omp_ros2/ros2_ws/install/setup.bash
 
 
 
 #---------------------------------------------INSTALL lidarslam_ros2---------------------------------------------
 
 
-#AB Install the slam repo and some more of its dependencies
+#FK Source the ndt_omp_ros2 package, since this workspace (lidarslam_ros2) depends on it, and therefore needs to source it before installation
+source ~/Apps/ndt_omp_ros2/ros2_ws/install/setup.bash
+
+#AB Install the lidarslam_ros2 package's source code
 cd ~/Apps/lidarslam_ros2/ros2_ws/src
 git clone --recursive https://github.com/rsasaki0109/lidarslam_ros2
 touch ~/Apps/lidarslam_ros2/ros2_ws/src/lidarslam_ros2/Thirdparty/ndt_omp_ros2/COLCON_IGNORE #AB Tell colcon to ignore the ndt_amp_ros2 package which comes bundled with the git repo
+
 cd ..
 rosdep install --from-paths src --ignore-src -r -y #AB Automatically install dependencies of the SLAM repo
 
+#FK Build the lidarslam_ros2 package
+cd ~/Apps/lidarlsam_ros2/ros2_ws #FK move to the root of the workspace, so that "colcon build" works correctly
 colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release #AB Colcon build the SLAM package
+
+source ~/Apps/lidarslam_ros2/ros2_ws/install/setup.bash #FK source lidarslam_ros2, so that it can be run soon if desired
 
 
 
