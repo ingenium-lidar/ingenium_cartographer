b20d067c2bbad446cef8a45654a3da0916773ebf
PurpleRabbit75 <abrahambaker104@gmail.com>
2026-07-21 16:19:47 +0300

    Added pauses and forced waits for get_ethernet_address.sh

--- Default_Apps_Installer.sh
+++ Default_Apps_Installer.sh
@@ -209,7 +209,8 @@ verbose_echo -e "$LIME Configuring ports and IP addresses...$NC "
 #AB This section rewrites your ethernet IP to be on the same network as the VLP-32C default. If your sensors are not connecting, you're probably on the wrong subnet.
 
 source ~/Documents/GitHub/ingenium_cartographer/agent_scripts/get_ethernet_address.sh #AB Run the get_ethernet_address.sh script to prompt the user for their ethernet port name and store it in a variable called ethernet. 
-nmcli connection add type ethernet ifname $ethernet con-name lidar-puck autoconnect yes ipv4.addresses "192.168.1.201" ipv4.method manual #FK Add a network connection to the ethernet port with the stable ipv4 address 192.168.1.100/24, which is necessary to connect to the VLP-32C LiDAR puck
+sudo nmcli connection add type ethernet ifname $ethernet con-name lidar-puck autoconnect yes ipv4.addresses "192.168.1.201" ipv4.method manual #FK Add a network connection to the ethernet port with the stable ipv4 address 192.168.1.100/24, which is necessary to connect to the VLP-32C LiDAR puck
+sleep 1
 
 
 
