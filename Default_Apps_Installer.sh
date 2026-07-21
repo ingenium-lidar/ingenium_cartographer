588195dae58d485d1400b4364cfa112f07a84b26
PurpleRabbit75 <abrahambaker104@gmail.com>
2026-07-21 20:26:16 +0300

    Updated SLAM echo

--- Default_Apps_Installer.sh
+++ Default_Apps_Installer.sh
@@ -274,7 +274,7 @@ rm veloview.tar.gz #AB delete the archive previously downloaded
 #---------------------------------------------INSTALL SLAM---------------------------------------------
 
 
-verbose_echo -e "$LIME Installing lidarslam_ros2...$NC "
+verbose_echo -e "$LIME Installing rsasaki0109's SLAM...$NC "
 cd ~/Documents/GitHub/ingenium_cartographer/agent_scripts/SLAM #AB Navigate to the ingenium_cartographer/agent_scripts directory. 
 ./Install_SLAM.sh #AB Install rsasaki0109's SLAM package. 
 # echo -e "\e[38;5;196m\033[1m DID NOT RUN SLAM INSTALLER. THE RELEVANT LINE OF CODE HAS BEEN COMMENTED UNTIL THE SCRIPT IS COMPLETE $NC "
